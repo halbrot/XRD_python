8eb5c7c77f61e3221d15320a9c8b2350d90ef252
hiroshi <halbrot1@gmail.com>
2020-01-17 11:24:50 +0900

    不要な変更削除

--- EvacCementite.py
+++ EvacCementite.py
@@ -8,7 +8,6 @@ Created on Thu Jan  9 14:12:45 2020
 import subprocess
 from subprocess import PIPE
 import shutil
-import time
 
 def topas(SampDisp, CrySizeFix, fixvalue=25):
     
@@ -132,7 +131,6 @@ class Application(tk.Frame):
     
     def pushed(self):
         self.label1["text"] = "F"
-        time.sleep(5)
         if self.var1.get()==True and self.txt.get()=="":
             self.label1["text"] = "CrySizeL の値を入力してください．"
         else:
